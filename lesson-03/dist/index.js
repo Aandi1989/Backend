74e801b2038c524b50e3bd21f2ea8b8b45a8a19e
Aandi1989 <navaliny1989@gmail.com>
2024-02-10 18:05:56 +0300

    09 foundation express, rest api, practice

--- lesson-03/dist/index.js
+++ lesson-03/dist/index.js
@@ -6,20 +6,67 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.get('/', (req, res) => {
-    const a = 4;
-    if (a > 5) {
-        res.send('Ok!');
+const HTTP_STATUSES = {
+    OK_200: 200,
+    CREATED_201: 201,
+    NO_CONTENT_204: 204,
+    BAD_REQUEST_400: 400,
+    NOT_FOUND_404: 404,
+};
+const jsonBodyMiddleware = express_1.default.json();
+app.use(jsonBodyMiddleware);
+const db = {
+    courses: [
+        { id: 1, title: 'front-end' },
+        { id: 2, title: 'back-end' },
+        { id: 3, title: 'automation qa' },
+        { id: 4, title: 'devops' }
+    ]
+};
+app.get('/courses', (req, res) => {
+    let foundedCourse = db.courses;
+    if (req.query.title) {
+        foundedCourse = foundedCourse.filter(c => c.title.indexOf(req.query.title) > -1);
     }
-    else {
-        res.send('Hello World!!!');
+    if (!foundedCourse.length) {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+        return;
     }
+    res.json(foundedCourse);
 });
-app.get('/samurais', (req, res) => {
-    res.send('Hello Samurais!!!');
+app.get('/courses/:id', (req, res) => {
+    const foundCourse = db.courses.find(c => c.id === +req.params.id);
+    if (!foundCourse) {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+        return;
+    }
+    res.json(foundCourse);
 });
-app.post('/samurais', (req, res) => {
-    res.send('We have created samurai');
+app.post('/courses', (req, res) => {
+    if (!req.body.title) {
+        res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
+        return;
+    }
+    const createedCourse = { id: +(new Date()), title: req.body.title };
+    db.courses.push(createedCourse);
+    res.status(HTTP_STATUSES.CREATED_201).json(createedCourse);
+});
+app.delete('/courses/:id', (req, res) => {
+    db.courses = db.courses.filter(c => c.id !== +req.params.id);
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+});
+app.put('/courses/:id', (req, res) => {
+    if (!req.body.title) {
+        res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
+        return;
+    }
+    const foundCourse = db.courses.find(c => c.id === +req.params.id);
+    if (!foundCourse) {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+        return;
+    }
+    foundCourse.title = req.body.title;
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
@@ -28,3 +75,4 @@ app.listen(port, () => {
 // yarn nodemon --inspect index.js получаем позможность дебажить
 // yarn nodemon --inspect .\dist\index.js запускаем преобразованный из ts в js файл  
 // yarn tsc -w запустить ts компилятор в режиме watcher
+// added commands yarn dev & yarn watch in package.json 
--- lesson-03/src/index.ts
+++ lesson-03/src/index.ts
@@ -3,22 +3,84 @@ import express from 'express';
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    const a = 4;
-    if(a > 5){
-        res.send('Ok!')
-    }else{
-        res.send('Hello World!!!')
+const HTTP_STATUSES = {
+    OK_200: 200,
+    CREATED_201: 201,
+    NO_CONTENT_204: 204,
+
+    BAD_REQUEST_400: 400,
+    NOT_FOUND_404: 404,
+}
+
+const jsonBodyMiddleware = express.json()
+app.use(jsonBodyMiddleware)
+
+const db = {
+    courses: [
+        {id:1, title: 'front-end'}, 
+        {id:2, title: 'back-end'}, 
+        {id:3, title: 'automation qa'}, 
+        {id:4, title: 'devops'}
+    ]
+}
+
+
+app.get('/courses', (req, res) => {
+    let foundedCourse = db.courses;
+
+    if(req.query.title){
+        foundedCourse = foundedCourse.filter(c => c.title.indexOf(req.query.title as string) > -1)
     }
+     
+    if(!foundedCourse.length){
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+        return;
+    }
+
+    res.json(foundedCourse)
+})
+app.get('/courses/:id', (req, res) => {
+    const foundCourse = db.courses.find(c => c.id === +req.params.id)
+    if(!foundCourse){
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+        return;
+    }
+
+    res.json(foundCourse)
+})
+app.post('/courses', (req, res) => {
+    if(!req.body.title){
+        res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400)
+        return;
+    }
+
+    const createedCourse = {id: +(new Date()), title: req.body.title}
+    db.courses.push(createedCourse)
+    res.status(HTTP_STATUSES.CREATED_201).json(createedCourse)
+})
+app.delete('/courses/:id', (req, res) => {
+    db.courses = db.courses.filter(c => c.id !== +req.params.id)
+    
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
+})
+app.put('/courses/:id', (req, res) => {
+    if(!req.body.title){
+        res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400)
+        return;
+    }
+
+    const foundCourse = db.courses.find(c => c.id === +req.params.id)
+    if(!foundCourse){
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+        return;
+    }
+
+    foundCourse.title = req.body.title
+
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
 })
 
-app.get('/samurais', (req, res) => {
-    res.send('Hello Samurais!!!')
-})
 
-app.post('/samurais', (req, res) => {
-    res.send('We have created samurai')
-})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
