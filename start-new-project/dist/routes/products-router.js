258339ead9dc0a0ceefb48655d38971b953d2af5
Aandi1989 <navaliny1989@gmail.com>
2024-02-16 12:58:45 +0300

    20 - Input validation, express validator

--- start-new-project/dist/routes/products-router.js
+++ start-new-project/dist/routes/products-router.js
@@ -3,31 +3,32 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.productsRouter = void 0;
 const express_1 = require("express");
 const products_repository_1 = require("../repositories/products-repository");
+const express_validator_1 = require("express-validator");
+const input_validation_middleware_1 = require("../midlewares/input-validation-middleware");
 exports.productsRouter = (0, express_1.Router)({});
-exports.productsRouter.get('/', (req, res) => {
-    // const foundProducts = productsRepository.findProducts(req.query.title?.toString());
-    // res.send(foundProducts)
-    // @ts-ignore
-    const blabla = req.blabla;
-    res.send({ value: blabla + "!!!" });
-});
-exports.productsRouter.post('/', (req, res) => {
+const titleValidation = (0, express_validator_1.body)('title').trim().isLength({ min: 3, max: 10 }).withMessage('Title length must be at least 3 characters.');
+exports.productsRouter.post('/', titleValidation, input_validation_middleware_1.inputValidationMiddleware, (req, res) => {
     const newProduct = products_repository_1.productsRepository.createProduct(req.body.title);
     res.status(201).send(newProduct);
 });
-exports.productsRouter.get('/:id', (req, res) => {
-    let product = products_repository_1.productsRepository.findProductById(+req.params.id);
-    if (product) {
+exports.productsRouter.put('/:id', titleValidation, input_validation_middleware_1.inputValidationMiddleware, (req, res) => {
+    const isUpdated = products_repository_1.productsRepository.updateProduct(+req.body.id, req.body.title);
+    if (isUpdated) {
+        const product = products_repository_1.productsRepository.findProductById(+req.body.id);
         res.send(product);
     }
     else {
         res.send(404);
     }
 });
-exports.productsRouter.put('/:id', (req, res) => {
-    const isUpdated = products_repository_1.productsRepository.updateProduct(+req.body.id, req.body.title);
-    if (isUpdated) {
-        const product = products_repository_1.productsRepository.findProductById(+req.body.id);
+exports.productsRouter.get('/', (req, res) => {
+    var _a;
+    const foundProducts = products_repository_1.productsRepository.findProducts((_a = req.query.title) === null || _a === void 0 ? void 0 : _a.toString());
+    res.send(foundProducts);
+});
+exports.productsRouter.get('/:id', (req, res) => {
+    let product = products_repository_1.productsRepository.findProductById(+req.params.id);
+    if (product) {
         res.send(product);
     }
     else {
--- start-new-project/src/routes/products-router.ts
+++ start-new-project/src/routes/products-router.ts
@@ -1,37 +1,46 @@
 import { Request, Response, Router, NextFunction } from "express";
 import { productsRepository } from "../repositories/products-repository";
+import { body, validationResult } from "express-validator";
+import { inputValidationMiddleware } from "../midlewares/input-validation-middleware";
 
 
 
 export const productsRouter = Router({});
 
+const titleValidation = body('title').trim().isLength({min: 3, max: 10}).withMessage('Title length must be at least 3 characters.')
 
 
-productsRouter.get('/', (req: Request, res: Response) => {
-    // const foundProducts = productsRepository.findProducts(req.query.title?.toString());
-    // res.send(foundProducts)
-    // @ts-ignore
-    const blabla = req.blabla;
-    res.send({value: blabla + "!!!"})
-})
-productsRouter.post('/', (req: Request, res: Response) => {
+
+productsRouter.post('/',
+     titleValidation,
+     inputValidationMiddleware,
+     (req: Request, res: Response) => {
+
     const newProduct = productsRepository.createProduct(req.body.title)
     res.status(201).send(newProduct)
+})
+productsRouter.put('/:id', 
+    titleValidation,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
+
+    const isUpdated = productsRepository.updateProduct(+req.body.id, req.body.title)
+    if(isUpdated){
+        const product = productsRepository.findProductById(+req.body.id);
+        res.send(product)
+    }else{
+        res.send(404)
+    }
+})
+productsRouter.get('/', (req: Request, res: Response) => {
+    const foundProducts = productsRepository.findProducts(req.query.title?.toString());
+    res.send(foundProducts)
 })
 productsRouter.get('/:id', (req: Request, res: Response) => {
     let product = productsRepository.findProductById(+req.params.id)
     if (product) {
         res.send(product)
     } else {
-        res.send(404)
-    }
-})
-productsRouter.put('/:id', (req: Request, res: Response) => {
-    const isUpdated = productsRepository.updateProduct(+req.body.id, req.body.title)
-    if(isUpdated){
-        const product = productsRepository.findProductById(+req.body.id);
-        res.send(product)
-    }else{
         res.send(404)
     }
 })
