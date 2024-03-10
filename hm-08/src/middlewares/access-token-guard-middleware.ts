625ed24e9801a808fde66f0f94cce113432a81ff
Aandi1989 <navaliny1989@gmail.com>
2024-03-10 18:15:36 +0300

    hm-08 completion after testing

--- hm-08/src/middlewares/access-token-guard-middleware.ts
+++ hm-08/src/middlewares/access-token-guard-middleware.ts
@@ -13,62 +13,36 @@ export const accessTokenGuard = async (req: Request,
 
     const accessToken = req.headers.authorization.split(' ')[1];
     const accessTokenData = await jwtService.getUserIdByToken(accessToken);
-    const refreshToken = req.cookies.refreshToken;
-    const refreshTokenData = await jwtService.getRefreshTokenData(refreshToken);
 
-    const refreshTokenCheckResult = await authService.checkRefreshToken(refreshToken);
+    // logic to create and renew refreshToken with each made request
+    // ------------------------------------------------
+    // const refreshToken = req.cookies.refreshToken;
+    // const refreshTokenData = await jwtService.getRefreshTokenData(refreshToken);
 
-    if(refreshTokenCheckResult.code !== ResultCode.Success) return res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401);
+    // const refreshTokenCheckResult = await authService.checkRefreshToken(refreshToken);
 
-    if(accessTokenData.userId || accessTokenData.message == 'jwt expired'){
-        const user = await usersQueryRepo.getUserById(accessTokenData.userId)
-        if(!user) return res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401);
+    // if(refreshTokenCheckResult.code !== ResultCode.Success) return res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401);
+
+    // if(accessTokenData.userId || accessTokenData.message == 'jwt expired'){
+    //     const user = await usersQueryRepo.getUserById(accessTokenData.userId)
+    //     if(!user) return res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401);
        
-        const newRefreshToken = await jwtService.createRefreshToken(accessTokenData.userId);
-        const revokedToken = await authService.revokeToken(refreshToken);
-        const addedToken = await authService.addToken(newRefreshToken);
+    //     const newRefreshToken = await jwtService.createRefreshToken(accessTokenData.userId);
+    //     const revokedToken = await authService.revokeToken(refreshToken);
+    //     const addedToken = await authService.addToken(newRefreshToken);
         
-        res.cookie('refreshToken', newRefreshToken.refreshToken, { httpOnly: true, secure: true }); 
+    //     res.cookie('refreshToken', newRefreshToken.refreshToken, { httpOnly: true, secure: true }); 
+
+    // --------------------------------------------------
+        if(accessTokenData){
+            const user = await usersQueryRepo.getUserById(accessTokenData.userId)
+
+        if(!user) return res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401);
+
+    //---------------------------------------------------
         req.user = user;
         return next();
     }
     return res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
 }
 
-// if(accessTokenData.userId){
-    //     const user = await usersQueryRepo.getUserById(accessTokenData.userId)
-    //     if(!user) return res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401);
-    //     const newRefreshToken = await jwtService.createRefreshToken(accessTokenData.userId);
-    //     const revokedToken = await authService.revokeToken(refreshToken);
-    //     const addedToken = await authService.addToken(newRefreshToken);
-    //     res.cookie('refreshToken', newRefreshToken.refreshToken, { httpOnly: true, secure: true }); 
-    //     req.user = user;
-    // }
-    // if(accessTokenData.message = 'jwt expired'){
-    //     if(refreshTokenData.message = 'jwt expired'){
-    //         const revokedToken = await authService.revokeToken(refreshToken);
-    //         return res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401);
-    //     }
-    //     if(refreshTokenData.userId){
-    //         const newRefreshToken = await jwtService.createRefreshToken(accessTokenData.userId);
-    //         const revokedToken = await authService.revokeToken(refreshToken);
-    //         const addedToken = await authService.addToken(newRefreshToken);
-    //         res.cookie('refreshToken', newRefreshToken.refreshToken, { httpOnly: true, secure: true }); 
-    //     }
-    // }
-    
-    // return next();  
-
-// if(Date.now() > accessTokenData.exp * 1000){
-//     const refreshToken = req.cookies.refreshToken;
-//     const refreshTokenData = await jwtService.getRefreshTokenData(refreshToken);
-//     if(Date.now() > refreshTokenData.exp * 1000){
-//         const revokedToken = await authService.revokeToken(refreshToken);
-//         return res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401);
-//     }else{
-//         const revokedToken = await authService.revokeToken(refreshToken);
-//         const newRefreshToken = await jwtService.createRefreshToken(accessTokenData.userId);
-//         const addedToken = await authService.addToken(newRefreshToken);
-//         res.cookie('refreshToken', newRefreshToken.refreshToken, { httpOnly: true, secure: true }); 
-//     }
-// }
