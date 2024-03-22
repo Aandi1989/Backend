18c5ef3de9af9abab5d78c23faee2c0bd38b9be0
Aandi1989 <navaliny1989@gmail.com>
2024-03-22 15:46:59 +0300

    hm-13 nestjs, create project, controllers, routing and services

---
+++ hm-13/src/users/users.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(protected usersService: UsersService) {}
+  @Get()
+  getUsers(@Query() query: { term: string }) {
+    return this.usersService.findUsers(query.term);
+  }
+  @Get(':id')
+  getUser(@Param('id') userId: string) {
+    return [{ id: 1 }, { id: 2 }].find((u) => u.id === +userId);
+  }
+  @Post()
+  createUser(@Body() inputModel: CreateUserInputModelType) {
+    return {
+      id: 12,
+      name: inputModel.name,
+      childrenCount: inputModel.childrenCount,
+    };
+  }
+  @Delete(':id')
+  deleteUser(@Param('id') userId: string) {
+    return [
+      { id: 1, name: 'Bob' },
+      { id: 2, name: 'Juan' },
+    ].filter((u) => u.id != +userId);
+  }
+  @Put(':id')
+  updateUser(
+    @Param('id') userId: string,
+    @Body() model: CreateUserInputModelType,
+  ) {
+    return {
+      id: userId,
+      model: model,
+    };
+  }
+}
+
+type CreateUserInputModelType = {
+  name: string;
+  childrenCount: number;
+};
